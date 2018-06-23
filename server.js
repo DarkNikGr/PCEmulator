73813a207223dcae9a0f6d6dbd526a9bba3a228d
DarkNik <mitakidisnikos@gmail.com>
2018-06-23 20:33:22 +0300

    fixes

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ class CPU extends HARDWARE {
                 if (task.cycles === 0) {
                     task.callback();
                 } else {
-                    let c = callback;
                     self.q.push(task);
                 }
                 callback();
@@ -73,65 +72,6 @@ class RAM extends HARDWARE {
         } else {
             this.pc.os.runningApps[app_id].exit('Error: RAM is full, program exit.');
         }
-    }
-}
-
-class OS {
-    constructor(pc) {
-        this.pc = pc;
-        this.wss_server = http.createServer();
-        this.wss = new WebSocket.Server({ server: this.wss_server });
-        this.wss.on('connection', this.new_terminal());
-        this.wss_server.listen(4578);
-
-        this.pc.ram.add('system', 297321);
-
-        this.runningApps = {};
-    }
-
-    new_terminal() {
-        let self = this;
-        return function(ws) {
-            console.log('connect');
-
-            let output = (output,) => {
-                ws.send(JSON.stringify(output));
-            };
-
-            ws.on('message', (cmd_data) => {
-                let data = JSON.parse(cmd_data);
-                if (data[0] === 'cmd') {
-                    if (data[1] === 0) {
-                        pc.os.cmd(data[2], output);
-                    } else {
-                        this.runningApps[data[1]].res = data[2];
-                    }
-                }
-            });
-        }
-    }
-
-    cmd(cmd, output) {
-        let args = cmd.split(" ");
-        if (this.pc.softwares[args[0]]) {
-            let software = this.pc.softwares[args[0]];
-            let app = new software(this.pc, args, output);
-            this.runningApps[app.index] = app;
-            this.cmd_change_app(app.index, output);
-            app.init();
-            if (app.status) app.main();
-        } else {
-            this.cmd_not_found(cmd, args, output);
-        }
-    }
-
-    cmd_change_app(index, output) {
-        output(['set_app', index]);
-    }
-
-    cmd_not_found(cmd, args, output) {
-        output(['echo',`Command ${args[0]} not found`]);
-        output(['exit', 0]);
     }
 }
 
@@ -188,15 +128,66 @@ class CMD_HI extends SOFTWARE {
     }
 }
 
+class OS {
+    constructor(pc) {
+        this.pc = pc;
+        this.wss_server = http.createServer();
+        this.wss = new WebSocket.Server({ server: this.wss_server });
+        this.wss.on('connection', this.new_terminal());
+        this.wss_server.listen(4578);
 
-class PC {
-    constructor(softwares) {
-        this.cpu = new CPU(this, 2, false, 3.4);
-        this.ram = new RAM(this, 2000000);
-        this.os = new OS(this);
-        this.softwares = softwares || {
+        this.softwares = {
             hi: CMD_HI
         };
+        this.runningApps = {};
+
+        this.pc.ram.add('system', 197321);
+    }
+
+    new_terminal() {
+        let self = this;
+        return function(ws) {
+            console.log('connect');
+
+            let output = (output,) => {
+                ws.send(JSON.stringify(output));
+            };
+
+            ws.on('message', (cmd_data) => {
+                let data = JSON.parse(cmd_data);
+                if (data[0] === 'cmd') {
+                    if (data[1] === 0) {
+                        self.cmd(data[2], output);
+                    } else {
+                        this.runningApps[data[1]].res = data[2];
+                    }
+                }
+            });
+        }
+    }
+
+    cmd(cmd, output) {
+        let args = cmd.split(" ");
+        if (this.softwares[args[0]]) {
+            let software = this.softwares[args[0]];
+            let app = new software(this.pc, args, output);
+            this.runningApps[app.index] = app;
+            app.init();
+            output(['set_app', app.index]);
+            if (app.status) app.main();
+        } else {
+            output(['echo',`Command ${args[0]} not found`]);
+            output(['exit', 0]);
+        }
+    }
+}
+
+
+class PC {
+    constructor() {
+        this.cpu = new CPU(this, 2, false, 2.6);
+        this.ram = new RAM(this, 2000000);
+        this.os = new OS(this);
     }
 }
 
